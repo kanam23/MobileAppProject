8fbb59fb1db55928c68765af949f528610bc7e4b
dhernandezortiz1 <dhernandezortiz1@student.gsu.edu>
2024-03-02 13:37:51 -0500

    View Flash Cards screen update
    Added widgets to "View Flash Cards" screen for the "front" and "back" of the cards with some sample questions and answers.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const FlashCardApp());
 
 class FlashCardApp extends StatelessWidget {
-  const FlashCardApp({super.key});
+  const FlashCardApp({super.key, Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -69,8 +69,44 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class ViewFlashCardsScreen extends StatelessWidget {
+class ViewFlashCardsScreen extends StatefulWidget {
   const ViewFlashCardsScreen({super.key});
+
+  @override
+  _ViewFlashCardsScreenState createState() => _ViewFlashCardsScreenState();
+}
+
+class _ViewFlashCardsScreenState extends State<ViewFlashCardsScreen> {
+  int _currentIndex = 0;
+
+  final List<Map<String, dynamic>> flashCards = [
+    {
+      'question': 'Which of the following animals can sleep with one eye open?',
+      'options': ['A. Giraffe', 'B. Elephant', 'C. Dolphin', 'D. Penguin'],
+      'correctOption': 'C. Dolphin',
+    },
+    {
+      'question': 'How many bones does an adult human have?',
+      'options': ['A. 206', 'B. 212', 'C. 196', 'D. 220'],
+      'correctOption': 'A. 206',
+    },
+    {
+      'question': 'What is the tallest building in the world?',
+      'options': [
+        'A. Burj Khalifa',
+        'B. Shanghai Tower',
+        'C. Abraj Al Bait Clock Tower',
+        'D. Taipei 101'
+      ],
+      'correctOption': 'A. Burj Khalifa',
+    },
+  ];
+
+  void _nextCard() {
+    setState(() {
+      _currentIndex = (_currentIndex + 1) % flashCards.length;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,8 +114,60 @@ class ViewFlashCardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('View Flash Cards'),
       ),
-      body: const Center(
-        child: Text('Sample Screen for Viewing Flash Cards'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Front of the flash card
+            Container(
+              width: 350,
+              height: 250,
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Fun Fact:',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    flashCards[_currentIndex]['question'],
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            // Back of the flash card with answer choices
+            Container(
+              width: 350,
+              height: 250,
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.green,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  for (final option in flashCards[_currentIndex]['options'])
+                    ElevatedButton(
+                      onPressed: () {},
+                      style:
+                          option == flashCards[_currentIndex]['correctOption']
+                              ? ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green)
+                              : null,
+                      child: Text(option),
+                    ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _nextCard,
+              child: const Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
