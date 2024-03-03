64512c3c05a094784add582e366b24d1ee009344
dhernandezortiz1 <dhernandezortiz1@student.gsu.edu>
2024-03-03 16:40:21 -0500

    Create Flash Cards Screen
    Added Widgets and Input fields for adding cards. The adding cards functionality doesn't work yet, will add SQL database functionality to keep the list of cards later.

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const FlashCardApp());
 
 class FlashCardApp extends StatelessWidget {
-  const FlashCardApp({super.key, Key? key});
+  const FlashCardApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -100,6 +100,21 @@ class _ViewFlashCardsScreenState extends State<ViewFlashCardsScreen> {
       ],
       'correctOption': 'A. Burj Khalifa',
     },
+    {
+      'question': 'What is the capital of France?',
+      'options': ['A. Madrid', 'B. London', 'C. Rome', 'D. Paris'],
+      'correctOption': 'D. Paris',
+    },
+    {
+      'question': 'Who is known as the father of modern physics?',
+      'options': [
+        'A. Isaac Newton',
+        'B. Albert Einstein',
+        'C. Galileo Galilei',
+        'D. Nikola Tesla'
+      ],
+      'correctOption': 'B. Albert Einstein',
+    },
   ];
 
   void _nextCard() {
@@ -173,8 +188,21 @@ class _ViewFlashCardsScreenState extends State<ViewFlashCardsScreen> {
   }
 }
 
-class CreateFlashCardsScreen extends StatelessWidget {
-  const CreateFlashCardsScreen({super.key});
+class CreateFlashCardsScreen extends StatefulWidget {
+  const CreateFlashCardsScreen({Key? key}) : super(key: key);
+
+  @override
+  _CreateFlashCardsScreenState createState() => _CreateFlashCardsScreenState();
+}
+
+class _CreateFlashCardsScreenState extends State<CreateFlashCardsScreen> {
+  final TextEditingController _frontController = TextEditingController();
+  final TextEditingController _backController1 = TextEditingController();
+  final TextEditingController _backController2 = TextEditingController();
+  final TextEditingController _backController3 = TextEditingController();
+  final TextEditingController _backController4 = TextEditingController();
+
+  int _correctAnswerIndex = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -182,10 +210,101 @@ class CreateFlashCardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Create Flash Cards'),
       ),
-      body: const Center(
-        child: Text('Sample Screen for Creating Flash Cards'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              // Front of the flash card input
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.only(bottom: 20.0),
+                child: TextField(
+                  controller: _frontController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter front of the flash card',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              // Back of the flash card input with four answer choices
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Back of the flash card (Answer choices)',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    for (int i = 0; i < 4; i++)
+                      Row(
+                        children: [
+                          Radio<int>(
+                            value: i,
+                            groupValue: _correctAnswerIndex,
+                            onChanged: (value) {
+                              setState(() {
+                                _correctAnswerIndex = value!;
+                              });
+                            },
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 10.0),
+                              child: TextField(
+                                controller: i == 0
+                                    ? _backController1
+                                    : i == 1
+                                        ? _backController2
+                                        : i == 2
+                                            ? _backController3
+                                            : _backController4,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter answer choice ${i + 1}',
+                                  border: const OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Logic for saving flash card will be implemented here
+                },
+                child: const Text('Save Flash Card'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _frontController.dispose();
+    _backController1.dispose();
+    _backController2.dispose();
+    _backController3.dispose();
+    _backController4.dispose();
+    super.dispose();
   }
 }
 
