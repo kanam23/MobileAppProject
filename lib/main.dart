09156b5594e13df87c4893193e749e0416a7f4c0
kanam23 <Khairula0903@gmail.com>
2024-03-04 17:17:42 -0500

    quiz screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,453 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const FlashCardApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlashCardApp extends StatelessWidget {
+  const FlashCardApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flash Card App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
+      routes: {
+        '/viewFlashCards': (context) => const ViewFlashCardsScreen(),
+        '/createFlashCards': (context) => const CreateFlashCardsScreen(),
+        '/takeQuiz': (context) => const TakeQuizScreen(),
+        '/congratulations': (context) => const CongratulationsScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/viewFlashCards');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text('View Flash Cards'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/createFlashCards');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text('Create Flash Cards'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/takeQuiz');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text('Take Quiz'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ViewFlashCardsScreen extends StatefulWidget {
+  const ViewFlashCardsScreen({super.key});
 
-  void _incrementCounter() {
+  @override
+  _ViewFlashCardsScreenState createState() => _ViewFlashCardsScreenState();
+}
+
+class _ViewFlashCardsScreenState extends State<ViewFlashCardsScreen> {
+  int _currentIndex = 0;
+
+  final List<Map<String, dynamic>> flashCards = [
+    {
+      'question': 'Which of the following animals can sleep with one eye open?',
+      'options': ['A. Giraffe', 'B. Elephant', 'C. Dolphin', 'D. Penguin'],
+      'correctOption': 'C. Dolphin',
+    },
+    {
+      'question': 'How many bones does an adult human have?',
+      'options': ['A. 206', 'B. 212', 'C. 196', 'D. 220'],
+      'correctOption': 'A. 206',
+    },
+    {
+      'question': 'What is the tallest building in the world?',
+      'options': [
+        'A. Burj Khalifa',
+        'B. Shanghai Tower',
+        'C. Abraj Al Bait Clock Tower',
+        'D. Taipei 101'
+      ],
+      'correctOption': 'A. Burj Khalifa',
+    },
+    {
+      'question': 'What is the capital of France?',
+      'options': ['A. Madrid', 'B. London', 'C. Rome', 'D. Paris'],
+      'correctOption': 'D. Paris',
+    },
+    {
+      'question': 'Who is known as the father of modern physics?',
+      'options': [
+        'A. Isaac Newton',
+        'B. Albert Einstein',
+        'C. Galileo Galilei',
+        'D. Nikola Tesla'
+      ],
+      'correctOption': 'B. Albert Einstein',
+    },
+  ];
+
+  void _nextCard() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentIndex = (_currentIndex + 1) % flashCards.length;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('View Flash Cards'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            // Front of the flash card
+            Container(
+              width: 350,
+              height: 250,
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Fun Fact:',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    flashCards[_currentIndex]['question'],
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            // Back of the flash card with answer choices
+            Container(
+              width: 350,
+              height: 250,
+              padding: const EdgeInsets.all(20.0),
+              color: Colors.green,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  for (final option in flashCards[_currentIndex]['options'])
+                    ElevatedButton(
+                      onPressed: () {},
+                      style:
+                          option == flashCards[_currentIndex]['correctOption']
+                              ? ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.green)
+                              : null,
+                      child: Text(option),
+                    ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _nextCard,
+              child: const Text('Next'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CreateFlashCardsScreen extends StatefulWidget {
+  const CreateFlashCardsScreen({Key? key}) : super(key: key);
+
+  @override
+  _CreateFlashCardsScreenState createState() => _CreateFlashCardsScreenState();
+}
+
+class _CreateFlashCardsScreenState extends State<CreateFlashCardsScreen> {
+  final TextEditingController _frontController = TextEditingController();
+  final TextEditingController _backController1 = TextEditingController();
+  final TextEditingController _backController2 = TextEditingController();
+  final TextEditingController _backController3 = TextEditingController();
+  final TextEditingController _backController4 = TextEditingController();
+
+  int _correctAnswerIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Flash Cards'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              // Front of the flash card input
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                margin: const EdgeInsets.only(bottom: 20.0),
+                child: TextField(
+                  controller: _frontController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter front of the flash card',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              // Back of the flash card input with four answer choices
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.green),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Back of the flash card (Answer choices)',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    for (int i = 0; i < 4; i++)
+                      Row(
+                        children: [
+                          Radio<int>(
+                            value: i,
+                            groupValue: _correctAnswerIndex,
+                            onChanged: (value) {
+                              setState(() {
+                                _correctAnswerIndex = value!;
+                              });
+                            },
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(bottom: 10.0),
+                              child: TextField(
+                                controller: i == 0
+                                    ? _backController1
+                                    : i == 1
+                                        ? _backController2
+                                        : i == 2
+                                            ? _backController3
+                                            : _backController4,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter answer choice ${i + 1}',
+                                  border: const OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Logic for saving flash card will be implemented here
+                },
+                child: const Text('Save Flash Card'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _frontController.dispose();
+    _backController1.dispose();
+    _backController2.dispose();
+    _backController3.dispose();
+    _backController4.dispose();
+    super.dispose();
+  }
+}
+
+class TakeQuizScreen extends StatefulWidget {
+  const TakeQuizScreen({super.key});
+
+  @override
+  _TakeQuizScreenState createState() => _TakeQuizScreenState();
+}
+
+class _TakeQuizScreenState extends State<TakeQuizScreen> {
+  int _currentIndex = 0;
+  int _score = 0;
+
+  final List<Map<String, dynamic>> quizQuestions = [
+    {
+      'question': 'Which of the following animals can sleep with one eye open?',
+      'options': ['A. Giraffe', 'B. Elephant', 'C. Dolphin', 'D. Penguin'],
+      'correctOption': 'C. Dolphin',
+    },
+    {
+      'question': 'How many bones does an adult human have?',
+      'options': ['A. 206', 'B. 212', 'C. 196', 'D. 220'],
+      'correctOption': 'A. 206',
+    },
+    {
+      'question': 'What is the tallest building in the world?',
+      'options': [
+        'A. Burj Khalifa',
+        'B. Shanghai Tower',
+        'C. Abraj Al Bait Clock Tower',
+        'D. Taipei 101'
+      ],
+      'correctOption': 'A. Burj Khalifa',
+    },
+    {
+      'question': 'What is the capital of France?',
+      'options': ['A. Madrid', 'B. London', 'C. Rome', 'D. Paris'],
+      'correctOption': 'D. Paris',
+    },
+    {
+      'question': 'Who is known as the father of modern physics?',
+      'options': [
+        'A. Isaac Newton',
+        'B. Albert Einstein',
+        'C. Galileo Galilei',
+        'D. Nikola Tesla'
+      ],
+      'correctOption': 'B. Albert Einstein',
+    },
+  ];
+
+  void _checkAnswer(String selectedOption) {
+    if (selectedOption == quizQuestions[_currentIndex]['correctOption']) {
+      setState(() {
+        _score++;
+      });
+    }
+    _nextQuestion();
+  }
+
+  void _nextQuestion() {
+    setState(() {
+      _currentIndex = (_currentIndex + 1) % quizQuestions.length;
+    });
+    if (_currentIndex == 0) {
+      Navigator.pushReplacementNamed(
+        context,
+        '/congratulations',
+        arguments: {'score': _score, 'totalQuestions': quizQuestions.length},
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Take Quiz'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Question ${_currentIndex + 1}/${quizQuestions.length}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              quizQuestions[_currentIndex]['question'],
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Column(
+              children: <Widget>[
+                for (final option in quizQuestions[_currentIndex]['options'])
+                  ElevatedButton(
+                    onPressed: () {
+                      _checkAnswer(option);
+                    },
+                    child: Text(option),
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CongratulationsScreen extends StatelessWidget {
+  const CongratulationsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> arguments =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    final int score = arguments['score'];
+    final int totalQuestions = arguments['totalQuestions'];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Congratulations'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Congratulations!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Your Score: $score/$totalQuestions',
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
